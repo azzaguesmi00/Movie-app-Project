c435e7d5e67806cc3b6970936d388e8620ac99be
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-10-09 19:20:25 +0100

    updatingl

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,24 +1,42 @@
-import React from 'react'
-import {Button , Card} from "react-bootstrap";
-import "./MOvieCard.css"
+import React from 'react';
+import { Button, Card } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import "./MOvieCard.css";
 
-const MovieCard = ({movie}) => {
+const MovieCard = ({ movie }) => {
+
+  // Function to display star rating
+  const Stars = (rate) => {
+    const rateStars = Math.min(rate, 5); // ensure the number of stars does not > 5
+
+    return (
+      <>
+        {}
+        {Array(rateStars).fill().map((_, index) => (
+          <FontAwesomeIcon key={index} icon={faStar} />
+        ))}
+      </>
+    );
+  };
+
   return (
     <div className='moviecard'>
-         <Card style={{ width: '18rem' }}>
-      <Card.Img className='img-card' variant="top" src={movie.posteURL} />
-      <Card.Body>
-        <Card.Title>{movie.titel}</Card.Title>
-        <Card.Text className='desc'>
-         {movie.description}</Card.Text>
-        <Card.Text>{movie.rate}</Card.Text> 
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-
-
+      <Card style={{ width: '18rem' }}>
+        <Card.Img className='img-card' variant="top" src={movie.posterUrl} />
+        <Card.Body>
+          <Card.Title>{movie.title}</Card.Title>
+          <Card.Text className='desc'>
+            {movie.description}
+          </Card.Text>
+          <div className='rating'>
+            {Stars(movie.rate)}  {/* Display star rating */}
+          </div>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     </div>
-  )
+  );
 }
 
-export default MovieCard
+export default MovieCard;
