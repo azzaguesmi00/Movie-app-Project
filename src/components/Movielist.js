264f773725200c48d34e590d97e773a64a41c56e
azzaguesmi00 <azzaguesmi77@gmail.com>
2024-10-09 18:20:14 +0100

    still

--- src/components/Movielist.js
+++ src/components/Movielist.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import MovieCard from './MovieCard'
-const Movielist = () => {
-    return {
-        <div className=''>
+const Movielist = ({movies}) => {
+    return (
+        <div className='movielist'>
+            {movies.map((el)=> (
+                <MovieCard movie={el} key = {el.id}/> 
+            ))}
+
         </div>
-    }
+    )
+
 }
    
 
